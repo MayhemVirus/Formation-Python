6537cc371e03e2b572a5e3d66f78f63bbc2b64c6
formation <formation@formation9.capen.sis>
2019-10-21 10:20:03 +0200

    Premier Exercice

---
+++ premier_script.py
@@ -0,0 +1,7 @@
+#!/bin/python3.6
+import random
+prenom = ["Paulin","Jeremy","Vincent"]
+animaux = ["L'ours","Le cochon","La baleine"]
+randprenom = random.choice(prenom)
+randanim = random.choice(animaux)
+print(randprenom + " " + randanim)
