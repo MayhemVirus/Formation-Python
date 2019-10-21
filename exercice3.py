b00e710e9cc12852b54c8ee84c851e7adc0e997e
MayhemVirus <formation@formation9.capen.sis>
2019-10-21 13:52:51 +0200

    Update ex3

--- exercice3.py
+++ exercice3.py
@@ -1,10 +1,10 @@
 #!/bin/python3.6
 chaine = "adroit#a3#vroom#b52#colorant#e111"
-print ("Voici la chaine de base:" + " " + chaine)
+print ("Voici la chaine de base :" + " " + chaine)
 separation = chaine.split("#")
 result = []
 for i in separation:
 	if i[-1].isdigit():
 		result.append(i)
 tab = ';'.join(result)
-print ("La chaine finale:" + " " + tab )
+print ("La chaine finale :" + " " + tab )
